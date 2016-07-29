5d4f6594f7e847dfffead5027d1434d0d4cd3911
Hasan Mehedi <mehedi.ou@gmail.com>
2016-07-28 23:18:26 -0400

    welcome screen is added

--- DesiRestaurantFinder/app/src/main/java/com/example/shaon/desirestaurantfinder/set_location.java
+++ DesiRestaurantFinder/app/src/main/java/com/example/shaon/desirestaurantfinder/set_location.java
@@ -2,6 +2,10 @@ package com.example.shaon.desirestaurantfinder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 public class set_location extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class set_location extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_location);
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.welcome_screen,(ViewGroup) findViewById(R.id.welcome));
+        Toast toast = new Toast(getApplicationContext());
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(layout);
+        toast.show();
     }
 }
